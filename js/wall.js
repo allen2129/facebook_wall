56343ea1cbf1eb605bd68f25f5859c00ca8a3dd6
allen2129 <jalle3930@withs.org>
2016-03-18 15:48:02 +0000

    Facebook wall project

--- js/wall.js
+++ js/wall.js
@@ -3,4 +3,7 @@ $(document).ready(function()
 	$('#message').focus();
 
 	// YOUR CODE GOES HERE
-});
+	$("button").click(function(){
+	$("wall").text();
+	$("src").append("http://www.scass.ae/en/About1/PublishingImages/Pages/staff/nophoto.jpg");
+	});
